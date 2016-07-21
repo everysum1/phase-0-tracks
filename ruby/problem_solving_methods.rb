49908aab7798f91c9b0d69ada00f3775bc4dc61b
Israel D. Matos <israeldmatos@gmail.com>
2016-07-21 14:45:05 -0700

    Add working solutions to search, fib, & insertion sort

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,70 @@
+#search array
+
+def search (arr, int)
+  found = nil
+  i = 0
+  while i < arr.length
+    if arr[i] == int
+      found = i
+    end
+  i += 1
+  end
+  found
+end
+
+#fibonacci
+
+def fib(int)
+  fibs = [0]
+  if int == 1
+    return fibs
+  elsif int == 2 
+    fibs << 1 
+    return fibs
+  elsif int == 3
+    fibs << 1 << 1
+    return fibs
+  else 
+    fibs << 1 << 1
+    while int > fibs.length 
+        fibs << fibs[-1] + fibs[-2] 
+    end
+  end
+  fibs 
+end
+
+
+#insertion sort
+
+
+#start with index 1
+#compare index with index-1
+#if greater than
+  #swap
+  #move left to next sorted number
+#otherwise
+  #stop
+  #move on to next index of unsorted numbers (right)
+#when index reaches array length, end program/display array
+
+
+
+def insertion(arr)
+  i = 1
+  while i < arr.length
+    j = i
+    while j > 0 
+      if arr[j-1] > arr[j] 
+        arr[j], arr[j-1] = arr[j-1], arr[j]
+      end
+      j = j-1
+    end
+    i += 1
+  end
+  arr
+end
+
+arr = [ 4, 7, 9, 11, 3, 5 ]
+puts insertion(arr).inspect
+
+  
