4d5f4527dc9ed099baab667725bdc5707ca0c719
Israel D. Matos <israeldmatos@gmail.com>
2016-07-28 23:41:10 -0700

    Add shout methods, classes with shout mixed in

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,10 +1,34 @@
 module Shout
 
-  def yell_angrily(words)
-    words + "!!!!" + "@/%*("
+  # def yell_angrily(words)
+  #   words + "!!!!" + "@/%*("
+  # end
+
+  # def yell_happily(words)
+  #   words + "!" + "  :)"
+  # end
+
+  def sales_pitch(product)
+    puts "Hey, wouldn't you like some nice fresh #{product.downcase}?? It's great, really..."
   end
 
-  def yell_happily(words)
-    words + "!" + "  :)"
+  def price_yeller(price)
+    puts "Hey now, get it right here, folks...only #{price.to_i} dollars. That's right!"
   end
 end
+
+class Promoter
+  include Shout
+
+end
+
+class FruitStandGuy
+  include Shout
+end 
+
+
+ralphie = FruitStandGuy.new
+ralphie.sales_pitch("Grapes")
+bryan = Promoter.new
+bryan.sales_pitch("comedy!")
+bryan.price_yeller("8")
