352807e32c85374a1411c236a58239777fb0fa17
Israel D. Matos <israeldmatos@gmail.com>
2016-08-06 22:40:48 -0700

    Add reflection

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -74,12 +74,48 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
-# # loop through each state in data hash
-# STATE_DATA.each do |state, info_hash|
-#   # initialize an object with key/value pair
-#   state_obj = VirusPredictor.new(state, info_hash[:population_density], info_hash[:population])
-#   # call virus effects method on that object
-#   state_obj.virus_effects
-# end
+# loop through each state in data hash
+STATE_DATA.each do |state, info_hash|
+  # initialize an object with key/value pair
+  state_obj = VirusPredictor.new(state, info_hash[:population_density], info_hash[:population])
+  # call virus effects method on that object
+  state_obj.virus_effects
+end
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+
+# # The hash syntax written with the rocket notation allows you to use a string as a key where as 
+# # the syntax with colon notation turns your key into a symbol.  Combining the two notations, 
+# # where the colon comes before the key and a hash rocket is used will also turn the key 
+# # into a symbol.  In terms of memory, symbols are more efficient since they point 
+# # to a place in memory where as a hash lookup with a string as a key will mean an 
+# # iteration over the string key in order to match it with the string reference for the look up. 
+
+# What does require_relative do? How is it different from require?
+
+# # Require relative allows you to include the code from another file in the global scope
+# # of the file that you are including it into.  It is different than require because
+# # it uses a relative path to the file it is included in, where as require uses an 
+# # absolute path from the root directory of the project or application it is located in.
+
+# What are some ways to iterate through a hash?
+
+# # You can iterate through a hash using the *each* method if your intent is to cause 
+# # a side effect, or you can use call the *times* method on the length of the keys
+# # array and use the iterator number as the index in the keys array to access the key,
+# # then use that key to access its respective value.  
+
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+
+# # What stood out the most was the fact that setting parameters for instance methods 
+# # that represented instance variables were completely unnecessary, since the instance 
+# # variables are available inside the method anyway, whether they are passed in or not.  
+
+# What concept did you most solidify in this challenge?
+
+# # How refactoring can be really easy if you just look at your code and notice where 
+# # you are repeating yourself unnecessarily and just abstract that out into its own 
+# # method.  Also, that instance variables are available anywhere in the class.  
+
